28cb541dae8d7da875ccbe27ba7937e32dc6c2c9
hakuunabatata <emachadomartins1999@gmail.com>
2021-10-20 07:45:58 -0300

    create message list component

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,59 @@
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+
 export const MessageList = () => (
   <>
-    <h1>Message List</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Mensagem 1 bla bla bla eu eu eu bla bla bla eu eu eu
+          </p>
+
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/hakuunabatata.png"
+                alt="Sandro Plankton"
+              />
+            </div>
+            <span>Sandro Plankton</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Mensagem 1 bla bla bla eu eu eu bla bla bla eu eu eu
+          </p>
+
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/hakuunabatata.png"
+                alt="Sandro Plankton"
+              />
+            </div>
+            <span>Sandro Plankton</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Mensagem 1 bla bla bla eu eu eu bla bla bla eu eu eu
+          </p>
+
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img
+                src="https://github.com/hakuunabatata.png"
+                alt="Sandro Plankton"
+              />
+            </div>
+            <span>Sandro Plankton</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   </>
 )
