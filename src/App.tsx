da0a46122bb3a1a6777aa8da9a19c5ce7e07f272
hakuunabatata <emachadomartins1999@gmail.com>
2021-10-21 07:40:04 -0300

    change signed backgound

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ export const App = () => {
   const { user } = useContext(AuthContext)
 
   return (
-    <main className={styles.contentWrapper}>
+    <main
+      className={`${styles.contentWrapper} ${
+        !!user ? styles.contentSigned : ''
+      }`}
+    >
       <MessageList />
       {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
